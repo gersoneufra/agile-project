e23f3a405191a9295d850ef20b8068157e449fbf
gersoneufra <gersoneufra@gmail.com>
2015-07-21 23:55:21 -0500

    up project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AgileProject::Application.config.secret_token = 'be24b519cac8fb3fe38e163f1a6782bbf7a3f657bb3a5e53b59a15cc6e860e58e3e5ac74b3d38903cd6025ba2f09c9146fa06b855b2bee39623a2f757d11d721'
